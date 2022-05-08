1c33af0e125bb7dbd82281bfb79551701bfda765
Armand Kamffer <akamffer@mweb.co.za>
2022-05-08 21:34:43 +0200

    Add basic unit test for CI purposes

---
+++ test/ui_constants_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mamba/ui_constants.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Colours', () {
+    test('primaryColor', () {
+      expect(primaryColor, const Color(0xFFA288E3));
+    });
+
+    test('darkPurple', () {
+      expect(darkPurple, const Color(0xFF38023B));
+    });
+
+    test('accentColor', () {
+      expect(accentColor, const Color(0xFFCEFDFF));
+    });
+
+    test('disabledColor', () {
+      expect(disabledColor, const Color.fromARGB(255, 116, 116, 116));
+    });
+
+    test('inputBackgroundColor', () {
+      expect(inputBackgroundColor, const Color.fromARGB(20, 0, 0, 0));
+    });
+  });
+}
