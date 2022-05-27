27200f04cd681d6d3b34a1fa6a552c65bf07ed58
Armand Kamffer <armand.kamffer@entelect.co.za>
2022-05-27 16:12:50 +0200

    Navigate to iOS folder

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+cd ios
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
